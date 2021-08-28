fc088b8208713a3944cf0a758accaac27e540b48
FriskyChuks <chuksedwin.ce@gmail.com>
2021-08-28 11:12:24 +0100

    updates for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn fleetmgt.wsgi
--- fleetmgt/settings.py
+++ fleetmgt/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'fleetmgt.urls'
@@ -137,9 +141,14 @@ STATICFILES_DIRS = (
     STATIC_DIR,
 )
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 GEOIP_PATH = os.path.join(BASE_DIR,'geoip')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+django_heroku.settings(locals())
