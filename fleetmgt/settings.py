57ca4db0f58d6cea9389c7c03ec004a7bd2988f5
FriskyChuks <chuksedwin.ce@gmail.com>
2021-08-31 13:50:13 +0100

    settings updated

--- fleetmgt/settings.py
+++ fleetmgt/settings.py
@@ -159,3 +159,22 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 django_heroku.settings(locals())
+
+CORS_REPLACE_HTTPS_REFERER      = True
+HOST_SCHEME                     = "https://"
+SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT             = True
+SESSION_COOKIE_SECURE           = True
+CSRF_COOKIE_SECURE              = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
+SECURE_HSTS_SECONDS             = 1000000
+SECURE_FRAME_DENY               = True
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
